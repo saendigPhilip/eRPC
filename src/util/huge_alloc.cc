8445ea0484cb66f96ba8878210d8471c1cef60be
saendigPhilip <philip.saendig@web.de>
2021-06-14 14:24:18 +0200

    Added option for compiling with SCONE

--- src/util/huge_alloc.cc
+++ src/util/huge_alloc.cc
@@ -1,16 +1,18 @@
 #include "huge_alloc.h"
+#include <iostream>
+#include "util/logger.h"
+
+#ifdef USE_SCONE
 #include <sys/mman.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <iostream>
-#include "util/logger.h"
-
 
 #define SYS_untrusted_mmap 1025
 static void * scone_kernel_mmap(void * addr, size_t length, int prot, int flags, int fd, off_t offset) {
   return (void*)syscall(SYS_untrusted_mmap, addr, length, prot, flags, fd, offset);
 }
+#endif // USE_SCONE
 
 namespace erpc {
 
@@ -70,6 +72,7 @@ void HugeAlloc::print_stats() {
 }
 
 
+#ifdef USE_SCONE
 Buffer HugeAlloc::alloc_raw(size_t size, DoRegister do_register) {
   std::cout << "SCONE_MMAP" << "\n";
   std::ostringstream xmsg;  // The exception message
@@ -142,6 +145,85 @@ Buffer HugeAlloc::alloc_raw(size_t size, DoRegister do_register) {
   return Buffer(shm_buf, SIZE_MAX,                                                      
       do_register_bool ? reg_info.lkey : UINT32_MAX);                       
 }
+#else // USE_SCONE
+
+Buffer HugeAlloc::alloc_raw(size_t size, DoRegister do_register) {
+  std::ostringstream xmsg;  // The exception message
+  size = round_up<kHugepageSize>(size);
+  int shm_key, shm_id;
+
+  while (true) {
+    // Choose a positive SHM key. Negative is fine but it looks scary in the
+    // error message.
+    shm_key = static_cast<int>(slow_rand.next_u64());
+    shm_key = std::abs(shm_key);
+
+    // Try to get an SHM region
+    shm_id = shmget(shm_key, size, IPC_CREAT | IPC_EXCL | 0666 | SHM_HUGETLB);
+
+    if (shm_id == -1) {
+      switch (errno) {
+        case EEXIST:
+          continue;  // shm_key already exists. Try again.
+
+        case EACCES:
+          xmsg << "eRPC HugeAlloc: SHM allocation error. "
+               << "Insufficient permissions.";
+          throw std::runtime_error(xmsg.str());
+
+        case EINVAL:
+          xmsg << "eRPC HugeAlloc: SHM allocation error: SHMMAX/SHMIN "
+               << "mismatch. size = " << std::to_string(size) << " ("
+               << std::to_string(size / MB(1)) << " MB).";
+          throw std::runtime_error(xmsg.str());
+
+        case ENOMEM:
+          // Out of memory - this is OK
+          ERPC_WARN(
+              "eRPC HugeAlloc: Insufficient hugepages. Can't reserve %lu MB.\n",
+              size / MB(1));
+          return Buffer(nullptr, 0, 0);
+
+        default:
+          xmsg << "eRPC HugeAlloc: Unexpected SHM malloc error "
+               << strerror(errno);
+          throw std::runtime_error(xmsg.str());
+      }
+    } else {
+      // shm_key worked. Break out of the while loop.
+      break;
+    }
+  }
+
+  uint8_t *shm_buf = static_cast<uint8_t *>(shmat(shm_id, nullptr, 0));
+  rt_assert(shm_buf != nullptr,
+            "eRPC HugeAlloc: shmat() failed. Key = " + std::to_string(shm_key));
+
+  // Mark the SHM region for deletion when this process exits
+  shmctl(shm_id, IPC_RMID, nullptr);
+
+  // Bind the buffer to the NUMA node
+  const unsigned long nodemask = (1ul << static_cast<unsigned long>(numa_node));
+  long ret = mbind(shm_buf, size, MPOL_BIND, &nodemask, 32, 0);
+  rt_assert(ret == 0,
+            "eRPC HugeAlloc: mbind() failed. Key " + std::to_string(shm_key));
+
+  // If we are here, the allocation succeeded.  Register if needed.
+  bool do_register_bool = (do_register == DoRegister::kTrue);
+  Transport::MemRegInfo reg_info;
+  if (do_register_bool) reg_info = reg_mr_func(shm_buf, size);
+
+  // Save the SHM region so we can free it later
+  shm_list.push_back(
+      shm_region_t(shm_key, shm_buf, size, do_register_bool, reg_info));
+  stats.shm_reserved += size;
+
+  // buffer.class_size is invalid because we didn't allocate from a class
+  return Buffer(shm_buf, SIZE_MAX,
+                do_register_bool ? reg_info.lkey : UINT32_MAX);
+}
+
+#endif // USE_SCONE
 
 
 Buffer HugeAlloc::alloc(size_t size) {
